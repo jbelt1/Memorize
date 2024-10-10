3cc7ecef613f6257b3c616702e8fd536705dfe53
Jeffrey Belt III <jbelt1@umd.edu>
2024-10-10 17:23:22 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jeffrey Belt III on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
